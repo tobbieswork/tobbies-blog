c0039402b61e83cdfe9d4ba827293349972cec14
Tobbies Nguyen <dangphat.job@gmail.com>
2024-08-22 11:17:23 +0700

    change app logic

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -1,59 +1,72 @@
+"use client";
 import Menu from "@/components/Menu/Menu";
 import styles from "./singlePage.module.css";
 import Image from "next/image";
 import Comments from "@/components/comments/Comments";
+import { useEffect, useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
-const getData = async (slug) => {
-  const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
-    cache: "no-store",
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed");
-  }
-
-  return res.json();
-};
-
-const SinglePage = async ({ params }) => {
-  const { slug } = params;
-
-  const data = await getData(slug);
+const SinglePage = ({ params }) => {
+  const [postData, setPostData] = useState(null);
+  const { status } = useSession();
+  const router = useRouter();
+  
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/login");
+    } else {
+      const getData = async (slug) => {
+        const res = await fetch(`http://localhost:3000/api/posts/${slug}`, {
+          cache: "no-store",
+        });
+      
+        if (!res.ok) {
+          throw new Error("Failed");
+        }
+        const data = await res.json();
+      
+        setPostData(data);
+      };
+      const { slug } = params;
+      getData(slug);
+    }
+  }, [status]);
 
   return (
-    <div className={styles.container}>
-      <div className={styles.infoContainer}>
-        <div className={styles.textContainer}>
-          <h1 className={styles.title}>{data?.title}</h1>
-          <div className={styles.user}>
-            {data.user?.image && (
+    postData && (
+      <div className={styles.container}>
+        <div className={styles.infoContainer}>
+          <div className={styles.textContainer}>
+            <h1 className={styles.title}>{postData?.title}</h1>
+            <div className={styles.user}>
               <div className={styles.userImageContainer}>
-                <Image src={data.user.image} alt="" fill className={styles.avatar} />
+                <Image src={postData.user.image || '/images/logo.jpg'} alt="" fill className={styles.avatar} />
               </div>
-            )}
-            <div className={styles.userTextContainer}>
-              <span className={styles.username}>{data?.user.name}</span>
-              <span className={styles.date}>{data.createdAt}</span>
+              <div className={styles.userTextContainer}>
+                <span className={styles.username}>{postData?.user.name}</span>
+                <span className={styles.date}>{postData.createdAt}</span>
+              </div>
             </div>
           </div>
-        </div>
-        <div className={styles.imageContainer}>
-          <Image src={data.img || '/images/development.jpg'} alt="" fill className={styles.image} />
-        </div>
-      </div>
-      <div className={styles.content}>
-        <div className={styles.post}>
-          <div
-            className={styles.description}
-            dangerouslySetInnerHTML={{ __html: data?.desc }}
-          />
-          <div className={styles.comment}>
-            <Comments postSlug={slug}/>
+          <div className={styles.imageContainer}>
+            <Image src={postData.img || '/images/development.jpg'} alt="" fill className={styles.image} />
           </div>
         </div>
-        <Menu />
+        <div className={styles.content}>
+          <div className={styles.post}>
+            <div
+              className={styles.description}
+              dangerouslySetInnerHTML={{ __html: postData?.desc }}
+            />
+            <div className={styles.comment}>
+              <Comments postSlug={postData.slug}/>
+            </div>
+          </div>
+          <Menu />
+        </div>
       </div>
-    </div>
+    )
   );
 };
 
